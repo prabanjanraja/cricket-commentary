3508bc452dcef0162d95111de1680b92c4f3e755
prabanjanraja <prabanjan.m@outlook.com>
2021-07-05 15:42:24 +0530

    loads Matches to schedule page

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class ScheduleRoute extends Route {
+  async model() {
+    let response = await fetch('api/schedule.json');
+    let { data } = await response.json();
+    let result = data.map((schedule) => {
+      let { attributes } = schedule;
+      return attributes;
+    });
+    console.log(result);
+    return result;
+  }
 }
