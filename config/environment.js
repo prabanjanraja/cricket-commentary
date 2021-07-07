218da7885dd053bf5080ac3fa6cc068717cf03ea
prabanjanraja <prabanjan.m@outlook.com>
2021-07-07 15:56:24 +0530

    updates to integrate with tomcat

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'cricket-commentary',
     environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: '/cricket_commentary',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
